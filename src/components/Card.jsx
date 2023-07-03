515d27e8639b9a9c127a17b9592422bb24afd4c0
Rubesh K R <rupesh.maxpain@gmail.com>
2023-07-03 20:40:49 +0530

    Add Details.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props) {
   return (
@@ -9,8 +10,10 @@ function Card(props) {
         <Avatar imgURL={props.imgURL} />
       </div>
       <div className="bottom">
-        <p className="info">{props.phone}</p>
-        <p className="info">{props.email}</p>
+        <Details info={props.phone} />
+        <Details info={props.email} />
+        {/* <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p> */}
       </div>
     </div>
   );
